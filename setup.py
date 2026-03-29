dc6641b271b8e1e0cbbec3097683b1f4126cf0b5
Alberto Daniel Badia <alberto_badia@enlacepatagonia.com>
2026-03-29 00:19:49 -0300

    IMPROVE: Add linux-to-manylinux2014 platform tag aliases in PlatformWheel class

--- setup.py
+++ setup.py
@@ -9,12 +9,19 @@ cmdclass = {}
 if _bdist_wheel is not None:
 
     class PlatformWheel(_bdist_wheel):
+        _PLATFORM_TAG_ALIASES = {
+            "linux_x86_64": "manylinux2014_x86_64",
+            "linux_i686": "manylinux2014_i686",
+            "linux_aarch64": "manylinux2014_aarch64",
+        }
+
         def finalize_options(self) -> None:
             super().finalize_options()
             self.root_is_pure = False
 
         def get_tag(self) -> tuple[str, str, str]:
             _python, _abi, platform_tag = super().get_tag()
+            platform_tag = self._PLATFORM_TAG_ALIASES.get(platform_tag, platform_tag)
             return ("py3", "none", platform_tag)
 
     cmdclass["bdist_wheel"] = PlatformWheel
